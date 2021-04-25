f6652e4f82f473d5d7d603968eb2dd17de2bafb0
Richelly Italo <richellyitalo@gmail.com>
2021-04-25 02:08:54 -0300

    ✨ Slider

---
+++ components/commons/slider.jsx
@@ -0,0 +1,32 @@
+import SliderLib from "react-slick";
+import 'slick-carousel/slick/slick.css';
+import 'slick-carousel/slick/slick-theme.css';
+import styled from 'styled-components';
+
+export default function Slider () {
+  const settings = {
+    dots: true,
+    infinite: true,
+    speed: 500,
+    slidesToShow: 1,
+    slidesToScroll: 1
+  };
+
+  const Img = styled.img`
+    width: 100%;
+    height: auto;
+  `
+
+  return (
+    <div className="container mx-auto text-center">
+      <SliderLib {...settings}>
+        <div >
+          <Img src="https://cdn.pixabay.com/photo/2020/12/10/09/22/beach-front-5819728_960_720.jpg" />
+        </div>
+        <div>
+          <Img src="https://cdn.pixabay.com/photo/2014/12/16/22/25/woman-570883_960_720.jpg" />
+        </div>
+      </SliderLib>
+    </div>
+  );
+}
--- pages/index.js
+++ pages/index.js
@@ -1,9 +1,14 @@
 import Navigation from '@/components/commons/navigation/navigation';
+import Slider from '@/components/commons/slider';
 import Head from 'next/head';
 import styles from '../styles/Home.module.css';
 
 export default function Home() {
   return (
-    <Navigation />
+    <div>
+      <Navigation />
+      <Slider />
+    </div>
   );
 }
+
